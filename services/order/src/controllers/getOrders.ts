45edcfc8e03f9c493565c962a1604f25b79df4aa
Dumpala Rushik <dumpala.rushik@mindfiresolutions.com>
2026-04-09 18:33:50 +0530

    fix: remove comments in the getorders file

--- services/order/src/controllers/getOrders.ts
+++ services/order/src/controllers/getOrders.ts
@@ -5,10 +5,7 @@ import prisma from "@/prisma";
 const getOrders = async (req: Request, res: Response, next: NextFunction) => {
   try {
 
-    //remove the where condition '{status:"COMPLETED"}' to get all orders instead of only completed orders
-    //before const orders = await prisma.order.findMany({where:{status:"COMPLETED"}})
-
-    //now it will return all orders regardless of their status
+    //fetch the orders
     const orders = await prisma.order.findMany();
     res.status(200).json(orders);
   } catch (error) {
